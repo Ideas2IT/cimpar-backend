e1ebcd7b6d016e93465a34a7d83cc96f07820710
MonishaSivanathan <monisha.sivanathan@ideas2it.com>
2024-07-03 11:36:32 +0530

    Custom Message - added controller, router, sample json file for custom message - done string change in auth controller - added all the neccessary constant for the file

---
+++ routes/custom_message_router.py
@@ -0,0 +1,22 @@
+import logging
+from fastapi import APIRouter, Request
+
+from utils.common_utils import permission_required
+from controller.custom_message_controller import CustomMessageClient
+
+
+router = APIRouter()
+logger = logging.getLogger("log")
+
+@router.get("/custom_message")
+@permission_required("MESSAGE", "READ")
+async def get_custom_message(request: Request):
+    logger.info(f"Request Payload: {request}")
+    return CustomMessageClient.get_custom_message()
+
+
+@router.post("/custom_message/{messge}")  
+@permission_required("MESSAGE", "WRITE")
+async def update_custom_message(message: str, request: Request):
+    logger.info(f"Request Payload: {message}")
+    return CustomMessageClient.update_custom_message(message)
