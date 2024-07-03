e708ac46a3bca7488792336315b48caf2933420a
MonishaSivanathan <monisha.sivanathan@ideas2it.com>
2024-07-03 11:36:32 +0530

    Custom Message - added controller, router, sample json file for custom message - done string change in auth controller - added all the neccessary constant for the file

---
+++ controller/custom_message_controller.py
@@ -0,0 +1,70 @@
+import os
+import json
+import logging
+import traceback
+from pathlib import Path
+
+from fastapi import status
+from fastapi.responses import JSONResponse
+from constants import MESSAGE_PATH
+
+logger = logging.getLogger("log")
+
+
+class CustomMessageClient:
+    @staticmethod
+    def get_custom_message():
+        cwd = os.getcwd()
+        relative_path = os.path.join(cwd, MESSAGE_PATH)
+        logger.info(f"Reading message from file: {relative_path}")
+        try:
+            with open(relative_path, "r") as file:
+                logger.info(f"Reading message from file: {relative_path}")
+                data = json.load(file)
+                logger.info(f"Message read from file: {data}")
+                message = data.get("message", "No message found")
+                return JSONResponse(
+                    status_code=status.HTTP_200_OK, content={"message": message}
+                )
+
+        except Exception as e:
+            logger.error(f"Error retrieving message: {str(e)}")
+            logger.error(traceback.format_exc())
+            error_response_data = {
+                "error": "Unable to read message",
+                "details": str(e),
+            }
+
+            return JSONResponse(
+                content=error_response_data, status_code=status.HTTP_400_BAD_REQUEST
+            )
+
+    @staticmethod
+    def update_custom_message(new_message: str):
+        cwd = os.getcwd()
+        relative_path = os.path.join(cwd, MESSAGE_PATH)
+        try:
+            with open(relative_path, "r") as file:
+                data = json.load(file)
+
+            data["message"] = new_message
+
+            with open(relative_path, "w") as file:
+                json.dump(data, file, indent=4)
+
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"message": "Message updated successfully."},
+            )
+
+        except Exception as e:
+            logger.error(f"Error updating message: {str(e)}")
+            logger.error(traceback.format_exc())
+            error_response_data = {
+                "error": "Unable to update message",
+                "details": str(e),
+            }
+
+            return JSONResponse(
+                content=error_response_data, status_code=status.HTTP_400_BAD_REQUEST
+            )
