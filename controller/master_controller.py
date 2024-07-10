3c8ce4a680dc2bef87be4acd4f55f54401eade77
MonishaSivanathan <monisha.sivanathan@ideas2it.com>
2024-07-10 17:13:07 +0530

    Master Table Added - added master table for race, state, ethnicity, lab_test

---
+++ controller/master_controller.py
@@ -0,0 +1,33 @@
+from fastapi import status, HTTPException
+from fastapi.responses import JSONResponse
+
+from services.aidbox_service import AidboxApi
+
+
+class MasterClient:
+    @staticmethod
+    def get_master_data(table_name: str):
+        table = {"race": "CimparRace", "state": "CimparState", "lab_test": "CimparLabTest",
+                 "ethnicity": "CimparEthnicity"}
+        if table_name not in table:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Kindly, verify the table name.")
+        try:
+            master_values = AidboxApi.make_request(
+                method="GET",
+                endpoint=f"/fhir/{table[table_name]}?_sort=.display"
+            )
+            values = master_values.json()
+            master_value = []
+            for entry in values["entry"]:
+                master_value.append(
+                    {
+                    "id": entry.get("resource", {}).get("id"), 
+                    "code": entry.get("resource", {}).get("code"),
+                    "display": entry.get("resource", {}).get("display")
+                    }
+                )
+            return master_value
+        except Exception as e:
+            return  JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=f"Failed to fetch data: {str(e)}")
