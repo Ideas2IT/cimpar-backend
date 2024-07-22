dda12415a153c955e2d906e9d56bd3836a813d17
Tamilselvan <tamilselvan.sudalai@ideas2it.com>
2024-07-22 22:40:13 +0530

    CIMPAR: Service history changes.   - Added custom query for service history.   - Added the pagination

--- routes/service_history_routes.py
+++ routes/service_history_routes.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Optional
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Query
 
 from utils.common_utils import permission_required
 from controller.service_history_controller import ServiceHistoryClient
@@ -14,14 +14,12 @@ logger = logging.getLogger("log")
 async def get_service_history_by_id(
     request: Request,
     patient_id: str,
-    page: int,
-    count: int,
-    all_service: Optional[bool] = None,
-    immunization: Optional[bool] = None,
-    lab_result: Optional[bool] = None,
-    name: Optional[str] = None,
+    service_type = None,
+    search_name: Optional[str] = None,
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1, le=100)
 ):
     logger.info(f"Service History ID:{patient_id}")
-    return ServiceHistoryClient.get_service_history_by_id(
-        patient_id, page, count, all_service, immunization, lab_result, name
+    return ServiceHistoryClient.get_service_history(
+        patient_id, service_type, search_name, page, page_size
     )
