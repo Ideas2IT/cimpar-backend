773736f407b610c12926fc86dc83f44873d154cf
MonishaSivanathan <monisha.sivanathan@ideas2it.com>
2024-07-30 11:30:57 +0530

    Fix Issues - modified create function in condition and allergy - convert into static method - adrressed few spelling issue and chaned

--- routes/appointment_routes.py
+++ routes/appointment_routes.py
@@ -36,17 +36,9 @@ async def get_by_id(
 async def get_appointment(
         request: Request,
         patient_name: Optional[str] = None,
-        state_date: Optional[str] = None,
+        start_date: Optional[str] = None,
         end_date: Optional[str] = None,
         service_name: Optional[str] = None,
         page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100)):
     logger.info(f"fetching values")
-    return AppointmentClient.get_appointment(patient_name, state_date, end_date, service_name, page, page_size)
-
-
-@router.put("/appointment/{appointment_id}")
-@permission_required("APPOINTMENT", "UPDATE")
-async def update_lab_status(appointment_id: str, update_status: StatusModel, request: Request):
-    logger.info(f"Appointment ID:{appointment_id}")
-    return AppointmentClient.update_appointment_status(appointment_id, update_status)
-
+    return AppointmentClient.get_appointment(patient_name, start_date, end_date, service_name, page, page_size)
