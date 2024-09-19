13fcadc145d6437e771d2a81a7543a2ca8968896
Tamilselvan Sudalai <106092208+tamsud@users.noreply.github.com>
2024-09-19 18:08:28 +0530

    Bumped up version to 1.0.13

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from routes import (insurance_routes, integration_pipeline_router, account_route
 # Load settings
 app = FastAPI(docs_url=None)
 
-CIMPAR_BE_VERSION = "1.0.12"
+CIMPAR_BE_VERSION = "1.0.13"
 
 # Configure CORS
 origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
